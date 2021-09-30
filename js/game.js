cc60a13f344fcf8756d4485080335112bf5e2461
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-09-30 19:57:06 +0200

    Setup player character

--- js/game.js
+++ js/game.js
@@ -6,4 +6,25 @@ function setupCanvas() {
   canvas.height = tileSize * numTiles
   canvas.style.width = canvas.width + "px"
   canvas.style.height = canvas.height + "px"
+
+  ctx.imageSmoothingEnabled = false
+}
+
+function drawSprite(sprite, x, y) {
+  ctx.drawImage(
+    spritesheet,
+    sprite * 16,
+    0,
+    16,
+    16,
+    x * tileSize,
+    y * tileSize,
+    tileSize,
+    tileSize
+  )
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawSprite(0, x, y)
 }
