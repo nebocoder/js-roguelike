e084fd46d07ab94a30b4f49ee0c35a3cb6ccc709
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-10-06 17:38:37 +0200

    Add sounds

--- js/monster.js
+++ js/monster.js
@@ -89,6 +89,12 @@ class Monster {
     if (this.hp <= 0) {
       this.die()
     }
+
+    if (this.isPlayer) {
+      playSound("hit1")
+    } else {
+      playSound("hit2")
+    }
   }
 
   die() {
--- js/tile.js
+++ js/tile.js
@@ -65,6 +65,7 @@ class Floor extends Tile {
   stepOn(monster) {
     if (monster.isPlayer && this.treasure) {
       score++
+      playSound("treasure")
       this.treasure = false
       spawnMonster()
     }
@@ -84,6 +85,7 @@ class Exit extends Tile {
 
   stepOn(monster) {
     if (monster.isPlayer) {
+      playSound("newLevel")
       if (level === numLevels) {
         addScore(score, true)
         showTitle()
